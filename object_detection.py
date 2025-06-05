3ee8e8960c8e2d2d1e4251c5d5b21a28079c510a
Mason <masonliang2@hotmail.com>
2025-06-05 16:22:29 -0700

    Basic yolo model

--- object_detection.py
+++ object_detection.py
@@ -1,10 +1,5 @@
-import tensorflow as tf
-from tensorflow import keras
-layers = tf.keras.layers
-import numpy as np
 import os
-import matplotlib.pyplot as plt
-
+from ultralytics import YOLO
 import kagglehub
 import shutil
 
@@ -21,3 +16,30 @@ if not os.path.exists(destination_path):
     print(f"Dataset copied to: {destination_path}")
 else:
     print(f"Dataset already exists at: {destination_path}")
+
+
+    
+
+# Load a pretrained YOLOv11 model (n = nano, s = small, m = medium, l = large)
+model = YOLO("yolo11n.pt")
+
+# Train the model
+model.train(
+    data="ocean.yaml",     # Path to your YAML config file
+    epochs=10,             # Number of training epochs
+    imgsz=160,             # Image size (can go higher if you have GPU power)
+    batch=64,              # Batch size (adjust based on your GPU RAM)
+    project="output", # Output folder
+    name="yolov8-ocean",     # Experiment name
+    exist_ok=True            # Overwrite if the folder already exists
+)
+
+predict_model = YOLO("output/yolov8-ocean/weights/best.pt")
+
+results = predict_model.predict(
+    source="aquarium-data-cots/aquarium_pretrain/test/images",  # folder of .jpg/.png images
+    conf=0.25,                     # confidence threshold
+    save=True,                     # saves images with boxes to disk
+    save_txt=True,                 # also saves YOLO-format predictions
+    name="yolo-test-output"        # folder inside runs/predict/
+)
